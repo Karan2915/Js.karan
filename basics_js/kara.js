87dd0e99753e729ce4cf5d409d7876fcf6c7f3d7
Karan2915 <kk29156506@gmail.com>
2025-05-09 02:20:33 +0530

    change in console

--- basics_js/kara.js
+++ basics_js/kara.js
@@ -1,1 +1,11 @@
-console.log("Karan a programmer");
+const accountId = 2915
+let accountEmail = "karan2915@gmail.com"
+let accountPassword = "karan2915"
+accountCity = "Jaipur"
+let accountCountry;
+
+accountEmail="kk29156506yahoo.com"
+accountPassword="k1223"
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountCountry])
