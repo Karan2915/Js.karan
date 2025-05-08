66deac13e215500fa236e7eb6ccdb394d9199935
Karan2915 <kk29156506@gmail.com>
2025-05-09 02:27:29 +0530

    change in file name

---
+++ basics_js/variables.js
@@ -0,0 +1,11 @@
+const accountId = 2915
+let accountEmail = "karan2915@gmail.com"
+let accountPassword = "karan2915"
+accountCity = "Jaipur"
+let accountCountry;
+
+accountEmail="kk29156506yahoo.com"
+accountPassword="k1223"
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountCountry])
