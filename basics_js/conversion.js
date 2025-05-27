7b3b16fb0cc7c26ac75b3c64d34c3fb91c1b9c01
Karan2915 <kk29156506@gmail.com>
2025-05-27 20:19:39 +0530

    change in code

--- basics_js/conversion.js
+++ basics_js/conversion.js
@@ -9,9 +9,29 @@ console.log(ValueNumber);
 let isloggedin = 1;
 let booleanisloggedin = Boolean(isloggedin);
 console.log(booleanisloggedin);
-*/
+
 
 let somenumber = 7;
 let StringNumber =String(somenumber);
 console.log(StringNumber);
 console.log(typeof StringNumber);
+*/
+let value = 10;
+let negativevalue = -value;
+//console.log(negativevalue);
+//console.log(20 + 12 + 11 + "21");
+//console.log("20" + 12 + 11 + 21);
+//console.log(20 + 12 + "11" + 21);
+//console.log(+true);
+//let gamecounter = 1000;
+//gamecounter++;
+//console.log(gamecounter)
+let x = 3;
+const y = x++;
+console.log(x);
+console.log(y);
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
---
+++ basics_js/operation.js
@@ -0,0 +1,13 @@
+let value = 3;
+let negativevalue = -value;
+//console.log(negativevalue);
+
+//console.log(value + negativevalue);
+//console.log(2**2);
+//console.log(2*3);
+//console.log(2+2);
+
+let str1 = "Hello";
+let str2 = " Karan";
+str3 = str1 + str2;
+console.log(str3);
